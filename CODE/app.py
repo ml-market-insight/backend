75b1999c8fa0b7a520d0fb647bf807b929e0d1b3
MatthieuVichet <matthieu.vichet@efrei.net>
2024-07-07 19:42:59 +0200

    Début test API

--- CODE/app.py
+++ CODE/app.py
@@ -1,16 +1,26 @@
 #### commande pour exécuter l'application : python BACKEND/CODE/app.py
 #### tester avec httpie : http GET http://127.0.0.1:5000/
 
-
+from db_connection import *
 
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 # Exemple de route pour tester l'API
-@app.route('/')
+@app.route('/fetchAllTickers')
 def index():
-    return jsonify({"message": "Bienvenue dans votre API Flask"})
+    
+    prediction = get_prediction_data()
+    pred = pd.DataFrame()
+
+    for i in range(len(prediction["ticker"])):
+        asset = prediction.loc[i, "ticker"]
+        time_series = pd.DataFrame(prediction.loc[i, "time_series_data"])
+        pred[asset] = time_series["close"]
+    
+
+    return jsonify({pred})
 
 # Exemple de route pour accéder à des données d'un modèle
 @app.route('/predict', methods=['POST'])
